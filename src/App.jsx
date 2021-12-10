efb19fa37c85c9efd2bbf1e0a6cc5695390f9dee
victor_arias <torvicarias@gmail.com>
2021-12-09 19:58:05 -0500

    Se construyen todas las formulas para los tipos mensual, trimestral, semestal y anual

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,38 @@ import { useState } from "react";
 
 function App() {
   const [number, setNumber] = useState("");
-  const resultado =
-    number !== "" ? ((1 + parseFloat(number) / 100) ** 12 - 1) * 100 : "";
+  const [tIngresada, setTIngresada] = useState("");
+  const [tSalida, setTSalida] = useState("");
+  let tasaEA = "";
+  let resultado = "";
+  
+  if (number !== "") {
+    if (tIngresada === "Mensual") {
+      tasaEA = (((1+number/100)**12)-1)*100
+    } else if (tIngresada === "Trimestral") {
+      tasaEA = (((1+number/100)**4)-1)*100
+    } else if (tIngresada === "Semestral") {
+      tasaEA = (((1+number/100)**2)-1)*100
+    } else if (tIngresada === "Anual") {
+      tasaEA = number
+    }
+  }
+
+  if (number !== "" && tasaEA !== "") {
+    if (tSalida === "Mensual") {
+      resultado = (((1+tasaEA/100)**(1/12))-1)*100
+    } else if (tSalida === "Trimestral") {
+      resultado = (((1+tasaEA/100)**(1/4))-1)*100
+    } else if (tSalida === "Semestral") {
+      resultado = (((1+tasaEA/100)**(1/2))-1)*100
+    } else if (tSalida === "Anual") {
+      resultado = tasaEA
+    }
+  }
+
+  if (tIngresada === tSalida) {
+    resultado = number
+  }
 
   return (
     <div className="d-flex justify-content-center mt-5">
@@ -21,21 +51,22 @@ function App() {
           <span>%</span>
         </div>
         <span>Seleccione el tipo de tasa ingresada</span>
-        <select>
+        <select onChange={(e) => setTIngresada(e.target.value)}>
+          <option disabled selected>Seleccione</option>
           <option>Mensual</option>
           <option>Trimestral</option>
           <option>Semestral</option>
           <option>Anual</option>
         </select>
         <span>Seleccione el tipo de tasa deseada</span>
-        <select>
+        <select onChange={(e) => setTSalida(e.target.value)}>
+          <option disabled selected>Seleccione</option>
           <option>Mensual</option>
           <option>Trimestral</option>
           <option>Semestral</option>
           <option>Anual</option>
         </select>
         <span>Resultado:</span>
-        {/* <input disabled value={resultado}></input> */}
         <div className="d-flex">
           <span>{resultado}</span>
           {number !== "" ? "%" : ""}
