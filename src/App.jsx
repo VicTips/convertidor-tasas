b7d59dd10cc86563cb12f63ba54759d57414a71a
victor_arias <torvicarias@gmail.com>
2021-12-12 19:48:42 -0500

    Se agregan estilos a los input, se incorpora font Open Sans, y se corrige error en la aparición del resultado

--- src/App.jsx
+++ src/App.jsx
@@ -7,70 +7,82 @@ function App() {
   const [tSalida, setTSalida] = useState("");
   let tasaEA = "";
   let resultado = "";
-  
+
   if (number !== "") {
     if (tIngresada === "Mensual") {
-      tasaEA = (((1+number/100)**12)-1)*100
+      tasaEA = ((1 + number / 100) ** 12 - 1) * 100;
     } else if (tIngresada === "Trimestral") {
-      tasaEA = (((1+number/100)**4)-1)*100
+      tasaEA = ((1 + number / 100) ** 4 - 1) * 100;
     } else if (tIngresada === "Semestral") {
-      tasaEA = (((1+number/100)**2)-1)*100
+      tasaEA = ((1 + number / 100) ** 2 - 1) * 100;
     } else if (tIngresada === "Anual") {
-      tasaEA = number
+      tasaEA = number;
     }
   }
 
   if (number !== "" && tasaEA !== "") {
     if (tSalida === "Mensual") {
-      resultado = (((1+tasaEA/100)**(1/12))-1)*100
+      resultado = ((1 + tasaEA / 100) ** (1 / 12) - 1) * 100;
     } else if (tSalida === "Trimestral") {
-      resultado = (((1+tasaEA/100)**(1/4))-1)*100
+      resultado = ((1 + tasaEA / 100) ** (1 / 4) - 1) * 100;
     } else if (tSalida === "Semestral") {
-      resultado = (((1+tasaEA/100)**(1/2))-1)*100
+      resultado = ((1 + tasaEA / 100) ** (1 / 2) - 1) * 100;
     } else if (tSalida === "Anual") {
-      resultado = tasaEA
+      resultado = tasaEA;
     }
   }
 
-  if (tIngresada === tSalida) {
-    resultado = number
+  if (tIngresada === tSalida && tIngresada !== "") {
+    resultado = number;
   }
 
   return (
-    <div className="d-flex justify-content-center mt-5">
+    <div className="d-flex justify-content-center mt-5 specialfont">
       <div className="d-flex flex-column">
         <span>Ingrese la tasa de interés</span>
-        <div className="d-flex align-items-center">
+        <div className="input-group mb-3">
           <input
             required
             placeholder="2.5"
-            className="text-center"
+            className="text-center form-control"
             onChange={(e) => setNumber(e.target.value)}
             value={number}
           ></input>
-          <span>%</span>
+          <span className="input-group-text">%</span>
         </div>
-        <span>Seleccione el tipo de tasa ingresada</span>
-        <select onChange={(e) => setTIngresada(e.target.value)}>
-          <option disabled selected>Seleccione</option>
+
+        <span>Tipo de tasa ingresada</span>
+        <select
+          onChange={(e) => setTIngresada(e.target.value)}
+          className="form-select mb-3"
+        >
+          <option disabled selected>
+            Seleccione
+          </option>
           <option>Mensual</option>
           <option>Trimestral</option>
           <option>Semestral</option>
           <option>Anual</option>
         </select>
-        <span>Seleccione el tipo de tasa deseada</span>
-        <select onChange={(e) => setTSalida(e.target.value)}>
-          <option disabled selected>Seleccione</option>
+
+        <span>Tipo de tasa deseada</span>
+        <select
+          onChange={(e) => setTSalida(e.target.value)}
+          className="form-select mb-3"
+        >
+          <option disabled selected>
+            Seleccione
+          </option>
           <option>Mensual</option>
           <option>Trimestral</option>
           <option>Semestral</option>
           <option>Anual</option>
         </select>
-        <span>Resultado:</span>
-        <div className="d-flex">
+
+        <h5 className="text-center">
           <span>{resultado}</span>
-          {number !== "" ? "%" : ""}
-        </div>
+          {resultado !== "" ? "%" : ""}
+        </h5>
       </div>
     </div>
   );
